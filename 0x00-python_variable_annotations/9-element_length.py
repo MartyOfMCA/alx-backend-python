59844a39df6ae8c6eef3b28607df5135b2a24f30
Martin <martyofmca@gmail.com>
2024-01-11 19:39:52 +0000

    add annotations to types - 0x00

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,19 +4,25 @@ Define a type-annotated method to return a
 tuple containing a list and it's length
 from a list of lists.
 """
+from typing import (
+        Tuple,
+        List,
+        Iterable,
+        Sequence
+        )
 
 
-def element_length(list_of_list):
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     Returns an object containing an element
     and it's length.
 
     Parameters:
-        list_of_list : list
+        lst : list
         A list whose elements are lists
 
     Returns:
         A tuple containing an element from
         `list_of_list` and its length.
     """
-    return [(i, len(i)) for i in list_of_list]
+    return [(i, len(i)) for i in lst]
