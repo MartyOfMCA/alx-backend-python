03c0beca6db5fa0462579b160b62f4f9b7cf400c
Martin <martyofmca@gmail.com>
2024-01-11 15:56:35 +0000

    define type-annotated float-to-string cast function - 0x00

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Define a type-annotated function to return
+the string representation for a float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation for
+    any given floating point value.
+
+    Parameters
+    n : float
+        The floating point number.
+
+    Return
+        The string representation
+        for the given value.
+    """
+    return (str(n))
