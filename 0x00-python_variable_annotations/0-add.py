cc78efd9c753ad1dfdf3eacd7fa8c2ba7e62c55e
Martin <martyofmca@gmail.com>
2024-01-11 15:32:25 +0000

    define type-annotated function to add two floats - 0x00

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Define a type-annotated function that returns
+the sum of two floating numbers.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add the two given floating point numbers.
+
+    Parameters
+    a : float
+        The first number.
+    b : float
+        The second number.
+
+    Return
+        The sum of the two floating
+        point numbers.
+    """
+    return (a + b)
