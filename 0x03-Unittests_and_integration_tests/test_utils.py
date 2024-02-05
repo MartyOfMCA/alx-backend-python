d0848e6282835c472f2a450dfc8bd8361ff3c86e
Martin <martyofmca@gmail.com>
2024-02-05 22:07:09 +0000

    add test method to handles exceptions when accessing nested map - 0x03

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -21,3 +21,15 @@ class TestAccessNestedMap(TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """ Test access nested map function. """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a", )),
+        ({"a": 1}, ("a", "b", )),
+        ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """
+        Test access nested map function with
+        exceptions.
+        """
+        with self.assertRaises(KeyError):
+            self.assertEqual(access_nested_map(nested_map, path), expected)
