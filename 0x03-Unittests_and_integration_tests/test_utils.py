00041db9f15b9e6e197248a5dfafc80743d37554
Martin <martyofmca@gmail.com>
2024-02-06 00:19:33 +0000

    define test to mock url request - 0x03

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,8 +4,13 @@ Define a test class to test functions in
 utils module.
 """
 from unittest import TestCase
+from unittest.mock import (
+        patch,
+        Mock
+        )
 from parameterized import parameterized
 from utils import access_nested_map
+from utils import get_json
 
 
 class TestAccessNestedMap(TestCase):
@@ -33,3 +38,23 @@ class TestAccessNestedMap(TestCase):
         """
         with self.assertRaises(KeyError):
             self.assertEqual(access_nested_map(nested_map, path), expected)
+
+
+class TestGetJson(TestCase):
+    """
+    Test class for the get json function.
+    """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+        ])
+    def test_get_json(self, url, payload):
+        """ Test get json function. """
+        mock = Mock()
+        mock.json.return_value = payload
+
+        with patch("requests.get", return_value=mock) as magic_mock:
+            response = get_json(url)
+            magic_mock.assert_called_once_with(url)
+            self.assertEqual(response, payload)
